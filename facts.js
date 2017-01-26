ab785072239df9c115291fa011506a0586a401b6
Niklas Sombert <niklas@ytvwld.de>
2017-01-26 14:43:49 +0100

    Facts.

---
+++ facts.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var facts = [
+	{
+		reason: "Da er noch Vitaminen enthält.",
+		source: "https://de.answers.yahoo.com/question/index?qid=20080205062005AAlwxc0"
+	},
+	{
+		reason: "Da er kein Zahnstocher ist.",
+		source: "https://de.answers.yahoo.com/question/index?qid=20080205062005AAlwxc0"
+	},
+	{
+		reason: "Da er noch nicht verwelkt ist.",
+		source: "https://de.answers.yahoo.com/question/index?qid=20080205062005AAlwxc0"
+	},
+	{
+		reason: "Da er noch keine Blüten hat.",
+		source: "http://www.gutefrage.net/frage/mein-brokoli-ist-nicht-mehr-komplett-gruen-sondern-schon-leicht-gelblich-noch-gut-oder-schlecht"
+	},
+	{
+		reason: "Da er noch nicht lange liegt.",
+		source: "http://www.gutefrage.net/frage/mein-brokoli-ist-nicht-mehr-komplett-gruen-sondern-schon-leicht-gelblich-noch-gut-oder-schlecht"
+	},
+	{
+		reason: "Da er keine Suppe ist.",
+		source: "http://www.gutefrage.net/frage/mein-brokoli-ist-nicht-mehr-komplett-gruen-sondern-schon-leicht-gelblich-noch-gut-oder-schlecht"
+	},
+	{
+		reason: "Da er noch schmeckt.",
+		source: "http://www.gutefrage.net/frage/mein-brokoli-ist-nicht-mehr-komplett-gruen-sondern-schon-leicht-gelblich-noch-gut-oder-schlecht"
+	},
+	{
+		reason: "Da er nicht schmierig ist.",
+		source: "http://www.gutefrage.net/frage/mein-brokoli-ist-nicht-mehr-komplett-gruen-sondern-schon-leicht-gelblich-noch-gut-oder-schlecht"
+	},
+	{
+		reason: "Da er als Beilage verwendet wird.",
+		source: "http://www.gutefrage.net/frage/mein-brokoli-ist-nicht-mehr-komplett-gruen-sondern-schon-leicht-gelblich-noch-gut-oder-schlecht"
+	},
+	{
+		reason: "Da er noch gut ist.",
+		source: "http://www.gutefrage.net/frage/mein-brokoli-ist-nicht-mehr-komplett-gruen-sondern-schon-leicht-gelblich-noch-gut-oder-schlecht"
+	}
+];
