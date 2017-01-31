343e65f01acc902529f2990eaadf9dabaebeec30
Niklas Sombert <niklas@ytvwld.de>
2017-01-31 12:13:17 +0100

    Make sure the next random fact isn't the same as the current one.

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ function showRandomFact()
 	var reason_elem = document.getElementById("reason");
 	
 	var rand_fact = facts[Math.floor(Math.random() * facts.length)];
-	
+	if (reason_elem.innerText == rand_fact.reason)
+	{
+		console.log("Next random fact would be the same as the current one. Skipping...");
+		return showRandomFact();
+	}
 	reason_elem.innerText = rand_fact.reason;
 }
 
