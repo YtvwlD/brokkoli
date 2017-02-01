1ae9a4d4112ed92d90e0fe8a84b8f53ea7d40efa
Niklas Sombert <niklas@ytvwld.de>
2017-02-01 12:44:37 +0100

    Use WTFPL instead of GPLv3.

--- facts.js
+++ facts.js
@@ -1,5 +1,12 @@
 "use strict";
 
+/*
+Copyright © 2017 Niklas Sombert <niklas@ytvwld.de>
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See the COPYING file for more details.
+*/
+
 var facts = [
 	{
 		reason: "Da er noch Vitaminen enthält.",
--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 "use strict";
 
+/*
+Copyright © 2017 Niklas Sombert <niklas@ytvwld.de>
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See the COPYING file for more details.
+*/
+
 function showRandomFact()
 {
 	var reason_elem = document.getElementById("reason");
